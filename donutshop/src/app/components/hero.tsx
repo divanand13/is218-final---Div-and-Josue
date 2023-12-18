a4d2d76439a7c8426bb0f681c192abd6e5ad5c6c
divanand13 <dsa2@njit.edu>
2023-12-18 03:42:16 -0500

    raised hero image opacity

--- donutshop/src/app/components/hero.tsx
+++ donutshop/src/app/components/hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className="container px-5 py-24 mx-auto flex flex-wrap">
           <div className="lg:w-2/3 mx-auto">
             <div className="flex flex-wrap w-full bg-gray-100 py-32 px-10 relative mb-4">
-              <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="/images/donut1.jpg"/>
+              <img alt="gallery" className="w-full object-cover h-full object-center block opacity-100 absolute inset-0" src="/images/donut1.jpg"/>
               <div className="text-center relative z-10 w-full">
               <div className="mb-80">
                 <h2 className="text-2xl text-black font-bold title-font">Last Call for Holiday Delights! 
