098b78ca2ab1d587601fa78bc5299000c088aa8a
divanand13 <dsa2@njit.edu>
2023-12-18 02:54:34 -0500

    created about us and location section under testimonials #9

--- donutshop/src/app/page.tsx
+++ donutshop/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Hero from '../app/components/hero'
 import Testimonials from './components/testimonials'
+import HeroNext from './components/hero2'
 export default function Home() {
   return (
    <main>
     <Hero />
     <Testimonials />
+    <HeroNext />
    </main> 
   )
 }
