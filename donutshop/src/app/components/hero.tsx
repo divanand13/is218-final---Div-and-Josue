e4944604f1b3b7d64786d105c12580fb4a59072e
divanand13 <dsa2@njit.edu>
2023-12-18 03:15:52 -0500

    inserted Pic for hero section #13

--- donutshop/src/app/components/hero.tsx
+++ donutshop/src/app/components/hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className="container px-5 py-24 mx-auto flex flex-wrap">
           <div className="lg:w-2/3 mx-auto">
             <div className="flex flex-wrap w-full bg-gray-100 py-32 px-10 relative mb-4">
-              <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://dummyimage.com/820x340"/>
+              <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="/images/donut1.jpg"/>
               <div className="text-center relative z-10 w-full">
                 <h2 className="text-2xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
                 <p className="leading-relaxed">Skateboard +1 mustache fixie paleo lumbersexual.</p>
