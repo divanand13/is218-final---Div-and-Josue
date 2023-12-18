3e1a40093eaa4f9dd0c0c546fdd06954c8e52ea9
divanand13 <dsa2@njit.edu>
2023-12-18 03:56:49 -0500

    testimonial bg color #32

--- donutshop/src/app/components/testimonials.tsx
+++ donutshop/src/app/components/testimonials.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Testimonials = () => {
     return (
-        <section className="text-gray-600 body-font">
+        <section className="text-gray-600 bg-pink-200 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
             <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
