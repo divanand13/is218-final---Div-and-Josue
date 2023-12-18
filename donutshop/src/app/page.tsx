140f53d02e863e009f30f71441ec92ded70fca5e
divanand13 <dsa2@njit.edu>
2023-12-18 02:45:29 -0500

    created testimonials section #7

--- donutshop/src/app/page.tsx
+++ donutshop/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import Hero from '../app/components/hero'
+import Testimonials from './components/testimonials'
 export default function Home() {
   return (
    <main>
     <Hero />
+    <Testimonials />
    </main> 
   )
 }
