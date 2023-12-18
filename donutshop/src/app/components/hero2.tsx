4368e07896193d930e85cf64d599e0aa10622b0a
divanand13 <dsa2@njit.edu>
2023-12-18 03:44:43 -0500

    location image opacity #26

--- donutshop/src/app/components/hero2.tsx
+++ donutshop/src/app/components/hero2.tsx
@@ -15,7 +15,7 @@ const HeroNext = () => {
               </div>
               <div className="px-2 w-1/2">
                 <div className="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
-                  <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="/images/location.jpg"/>
+                  <img alt="gallery" className="w-full object-cover h-full object-center block opacity-50 absolute inset-0" src="/images/location.jpg"/>
                   <div className="text-center relative z-10 w-full">
                     <h2 className="text-xl text-gray-900 font-medium title-font mb-2">Location</h2>
                     <p className="leading-relaxed">TheDonutDen is in service at NJIT! Come stop by at 250 Central Avenue.</p>
