603e7924a4a4e4cc108336f195f370c69487a2fd
divanand13 <dsa2@njit.edu>
2023-12-18 03:22:01 -0500

    inserted About Us info and text #15

--- donutshop/src/app/components/hero2.tsx
+++ donutshop/src/app/components/hero2.tsx
@@ -7,8 +7,8 @@ const HeroNext = () => {
                 <div className="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
                   <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://dummyimage.com/542x460"/>
                   <div className="text-center relative z-10 w-full">
-                    <h2 className="text-xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
-                    <p className="leading-relaxed">Skateboard +1 mustache fixie paleo lumbersexual.</p>
+                    <h2 className="text-xl text-gray-900 font-medium title-font mb-2">About Us</h2>
+                    <p className="leading-relaxed">Founded by two college friends with a shared passion for baking and entreprenuership.</p>
                     <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
                       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
