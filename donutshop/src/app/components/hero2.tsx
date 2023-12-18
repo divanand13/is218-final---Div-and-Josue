db9d4c37c4ab8da25223fd613f441f143f8beccc
divanand13 <dsa2@njit.edu>
2023-12-18 03:25:07 -0500

    added aboutus pic

--- donutshop/src/app/components/hero2.tsx
+++ donutshop/src/app/components/hero2.tsx
@@ -5,7 +5,7 @@ const HeroNext = () => {
         <div className="flex flex-wrap -mx-2">
               <div className="px-2 w-1/2">
                 <div className="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
-                  <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://dummyimage.com/542x460"/>
+                  <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="/images/aboutus.jpg"/>
                   <div className="text-center relative z-10 w-full">
                     <h2 className="text-xl text-gray-900 font-medium title-font mb-2">About Us</h2>
                     <p className="leading-relaxed">Founded by two college friends with a shared passion for baking and entreprenuership.</p>
