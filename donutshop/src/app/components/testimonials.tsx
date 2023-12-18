736cfa87ef2a329ede1d06fff2e518d2ed447b8d
divanand13 <dsa2@njit.edu>
2023-12-18 03:49:46 -0500

    testimonial 3 image size #29

--- donutshop/src/app/components/testimonials.tsx
+++ donutshop/src/app/components/testimonials.tsx
@@ -25,7 +25,7 @@ const Testimonials = () => {
             </div>
             <div className="lg:w-1/3 lg:mb-0 p-4">
               <div className="h-full text-center">
-                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="/images/testimonial3.jpg"/>
+                <img alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="/images/testimonial3.jpg"/>
                 <p className="leading-relaxed">"As a quantum physicist and donut enthusiast, I can confirm that TheDonutDen exists in a flavor dimension of its own. The donuts defy the laws of taste with every bite, and I suspect they've mastered the art of interdimensional glazing. The 'Einstein's Espresso Explosion' donut made me question the fabric of reality. TheDonutDen is the portal to flavor universes unknown!"</p>
                 <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
                 <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">SUE BIRD</h2>
