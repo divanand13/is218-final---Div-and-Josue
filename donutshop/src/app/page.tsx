d17e25c6da72ecead9809fead454c22c6d6680ce
divanand13 <dsa2@njit.edu>
2023-12-18 23:35:23 -0500

    removed cookie consent import #53

--- donutshop/src/app/page.tsx
+++ donutshop/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Hero from '../app/components/hero'
 import Testimonials from './components/testimonials'
 import HeroNext from './components/hero2'
-import CookieConsent from './components/CookieConsent'
+
 export default function Home() {
   return (
    <main>
