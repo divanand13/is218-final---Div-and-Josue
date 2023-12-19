67ee8042cab15a30d325afbab676959a272881f8
divanand13 <dsa2@njit.edu>
2023-12-18 19:43:33 -0500

    mailchimp attempt 3

--- donutshop/src/app/components/NewsLetterForm.js
+++ donutshop/src/app/components/NewsLetterForm.js
@@ -4,7 +4,7 @@ import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 const NewsletterForm = () => {
   return (
-    <MailchimpSubscribe url={process.env.NEXT_PUBLIC_MAILCHIMP_URL} />
+    <MailchimpSubscribe url= 'https://njit.us13.list-manage.com/subscribe/post?u=3f0bef50ba585909966342932&amp;id=80d56cc6ec&amp;f_id=0071ede7f0' />
   );
 };
 
